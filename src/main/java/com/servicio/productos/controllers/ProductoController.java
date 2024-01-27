e3e490c8cbf3e33e5337c3865f8e2b770870416a
danicode <danielanzawa@gmail.com>
2024-01-27 19:20:05 -0300

    Utilizando Ribbon para balanceo de carga en RestTemplate

--- src/main/java/com/servicio/productos/controllers/ProductoController.java
+++ src/main/java/com/servicio/productos/controllers/ProductoController.java
@@ -3,6 +3,7 @@ package com.servicio.productos.controllers;
 import com.servicio.productos.models.entity.Producto;
 import com.servicio.productos.models.service.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,11 @@ import java.util.stream.Collectors;
 @RestController // una de las cosas que hace esta anotacion es convertir a json lo q retorna el metodo handler
 public class ProductoController {
 
-    @Autowired
-    private Environment env;
+    //@Autowired
+    //private Environment env;
+
+    @Value("${server.port}")
+    private Integer port;
 
     @Autowired
     private IProductoService productoService;
@@ -23,7 +27,8 @@ public class ProductoController {
     @GetMapping("/listar")
     public List<Producto> listar() {
         return productoService.findAll().stream().map(producto -> {
-            producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+            //producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+            producto.setPort(port);
             return producto;
         }).collect(Collectors.toList());
     }
@@ -31,7 +36,8 @@ public class ProductoController {
     @GetMapping("/ver/{id}")
     public Producto detalle(@PathVariable Long id) {
         Producto producto = productoService.findById(id);
-        producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+        //producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+        producto.setPort(port);
         return producto;
     }
 }
