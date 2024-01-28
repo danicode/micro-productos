09099c6b37f9462d55e4d7af0b3ad81146bc2ff6
danicode <danielanzawa@gmail.com>
2024-01-28 15:30:22 -0300

    Conectando nuestros servicios como clientes Eureka

--- src/main/java/com/servicio/productos/ProductosApplication.java
+++ src/main/java/com/servicio/productos/ProductosApplication.java
@@ -2,7 +2,9 @@ package com.servicio.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductosApplication {
 
